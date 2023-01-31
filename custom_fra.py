4d6798fc15042391f2a133e31860e6d6c5e13684
reps <marco@reps.cc>
2023-02-01 00:36:55 +0100

    Update custom_fra.py

--- custom_fra.py
+++ custom_fra.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+input_chan = 3
+
 import vxi11
 inst = vxi11.Instrument("TCPIP::192.168.178.31::INSTR")
 print(inst.ask("*IDN?"))
 inst.clear()
 inst.write("*RST")
-inst.write("WGENerator1:PRESet.")
+inst.write("WGENerator1:PRESet")
+inst.write("CHANnel1:STATe OFF")
+inst.write("CHANnel"+input_chan+":STATe ON")
