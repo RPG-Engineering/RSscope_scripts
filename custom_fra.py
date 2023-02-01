56f55ca4b2962434b823b3ca65d5baaeb4b9f80d
reps <marco@reps.cc>
2023-02-01 13:00:03 +0100

    Update custom_fra.py

--- custom_fra.py
+++ custom_fra.py
@@ -29,6 +29,7 @@ inst.write("MEASurement1:SOURce C"+input_chan)
 inst.write("WGENerator1:VOLTage:VPP "+Amplitude)
 inst.write("WGENerator1:ENABle ON")
 
+timestr = time.strftime("%Y%m%d-%H%M%S_")
 with open('csv/'+timestr+'MXO44vsOldPreamp', mode='w') as csv_file:
     fieldnames = ['freq', 'Vppin', 'Vppout']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
